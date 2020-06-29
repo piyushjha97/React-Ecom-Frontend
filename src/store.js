bb2234f60a45c95471891f750f49095cd09686ef
8bithemant <hd4207577@gmail.com>
2020-06-29 23:45:31 +0530

    Reducer action and store initialised

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import {createStore, combineReducers} from 'redux'
+
+
+
+const initialState={};
+const reducer= combineReducers({
+    productList:productListReducer,
+})
+
+const store = createStore(reducer, initialState);
