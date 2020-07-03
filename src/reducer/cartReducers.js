c5b78af853e3e37abaae0ef49c95ffe08f8d194b
8bithemant <hd4207577@gmail.com>
2020-07-03 22:33:22 +0530

    cart reducer up

--- src/reducer/cartReducers.js
+++ src/reducer/cartReducers.js
@@ -1,10 +1,19 @@
 
 
-function cartReducer(state={cartItem:[]}, action){
+function cartReducer(state={cartItems:[]}, action){
     switch (Action.type) {
         case ADD_TO_CART: 
         const item = action.payload;
-        const product=state.cartItem.find(x=>x.product ===item.product)
+        const product = state.cartItems.find(x=>x.product === item.product)
+
+         if(product){
+             return{
+                 cartItem:
+                    state.cartItems.map(x=> x.product === product.product? item:x)
+             }
+         }
+
+         return{cartItems:[...state.cartItems,item]}
             
             
     
